9b6d4ea8b59822ebc4e26445999a219a3ff81c60
Anne <144004647+Annevd@users.noreply.github.com>
2024-09-10 10:00:43 +0200

    V1: Reset ontwikkelomgeving
    Na het openen van de meest recente commit, kon de website niet meer openen met npm run dev, dus heb ik alles verwijderd en opnieuw geïnstalleerd, precies zoals mijn vorige commit. Wat opvalt is dat er in deze versie een versie verandering is?

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-  export let data
-</script>
-
-<main>
-<h1>Anne</h1>
-<p>{data.person.name}</p>
-<p>{data.person.bio}</p>
-</main>
+    export let data
+  </script>
+  
+  <main>
+  <h1>Anne</h1>
+  <p>{data.person.name}</p>
+  <p>{data.person.bio}</p>
+  </main>
